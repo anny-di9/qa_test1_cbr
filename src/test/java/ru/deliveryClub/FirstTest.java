f04f779b56e7c1e727821f429d8fe9ae659a0420
Anny-di9 <a394216@mail.ru>
2020-11-08 15:00:44 +0300

    CBR

---
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -0,0 +1,22 @@
+package ru.deliveryClub;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest() {
+
+        String projectPath = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver", projectPath + "//drivers/chromedriver.exe");
+        ChromeDriver driver= new ChromeDriver();
+        driver.get("https://www.cbr.ru/");
+
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("Центральный банк Российской Федерации | Банк России"));
+        driver.quit();
+
+    }
+}
